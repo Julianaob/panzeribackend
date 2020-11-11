36b9afb79d8e1a7203dd68800ed3d022622ff064
Marcelino <santanatruocchio@gmail.com>
2020-11-10 21:23:59 -0300

    add index

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const PORT = 3000
+const HOST = '0.0.0.0'
+
+const app = express()
+
+app.get('/', (req, res) =>{
+    res.send('Hello World Home Panzeri')
+})
+
+
+app.get('/evaluation', (req, res) =>{
+    res.send('Hello ')
+})
+
+
+app.listen(PORT, HOST)
